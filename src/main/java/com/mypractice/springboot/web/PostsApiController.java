6caf71f639d7a5272575507ac1a5fde8c15bf7d7
sonyg11847 <sonyg11847@gmail.com>
2023-12-02 22:28:31 +0900

    update PostsApiController

---
+++ src/main/java/com/mypractice/springboot/web/PostsApiController.java
@@ -0,0 +1,31 @@
+package com.mypractice.springboot.web;
+
+import com.mypractice.springboot.service.posts.PostsService;
+import com.mypractice.springboot.web.dto.PostsResponseDto;
+import com.mypractice.springboot.web.dto.PostsSaveRequestDto;
+import com.mypractice.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("api/v1/posts/{id}")
+    public Long update(@PathVariable Long id,
+                       @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
